5cb47fa2d332ee1ffd238ccdff288e826ed12af4
EduardoPateis <epjoaquim@outlook.com>
2023-05-29 22:14:44 -0300

    Ajustando o swagger

--- swagger/autoGenDoc.js
+++ swagger/autoGenDoc.js
@@ -1,13 +1,10 @@
 const mongooseToSwagger = require('mongoose-to-swagger');
-// const EsquemaTarefa = require('../scr/model/tarefa.js');
-// const EsquemaUsuario = require('../scr/model/usuario.js');
-const swaggerAutogen = require('swagger-autogen')
-({
+const swaggerAutogen = require('swagger-autogen')({
     opneapi:'3.0.0',
     language: 'pt-BR',
-})
+});
 const outputFile = './swagger_output.json';
-const endpointsFiles = ['../index.js' /*,'../src/routes.js' */];
+const endpointsFiles = ["../index.js"]; 
 
 let doc ={
     info:{
@@ -26,23 +23,9 @@ let doc ={
     }
     ],
     consumes: ['application/json'],
-    produces: ['application/json']//,
-    // securityDefinitions:{
-    //     api_key:{
-    //         type:"apiKey",
-    //         name:"api_key",
-    //         in:"header"
-    //     }
-
-    // },
-    // components:{
-    //     schemas:{
-    //         Usuario:mongooseToSwagger(EsquemaUsuario),
-    //         Tarefa: mongooseToSwagger(EsquemaTarefa),
-    //     }
-    // }
+    produces: ['application/json']
 }
-swaggerAutogen:(outputFile, endpointsFiles, doc).then( () => {
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
     console.log("Documentação do Swagger gerada encontra-se no arquivo em: "+ outputFile);
     if(process.env.NODE_ENV !== 'production'){
         require("../index.js");
