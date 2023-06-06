e26ae21497ad3f511d5cc4e2dea72bad88d1fd23
EduardoPateis <epjoaquim@outlook.com>
2023-06-06 20:33:35 -0300

    Correção do swagger
    Sofrido mais consegui achar o erro

--- swagger/autoGenDoc.js
+++ swagger/autoGenDoc.js
@@ -1,34 +1,51 @@
 const mongooseToSwagger = require('mongoose-to-swagger');
+//const EsquemaTarefa = require('../src/models/tarefa.js');
+//const EsquemaUsuario = require('../src/models/usuario.js');
 const swaggerAutogen = require('swagger-autogen')({
-    opneapi:'3.0.0',
+    openapi: '3.0.0',
     language: 'pt-BR',
 });
-const outputFile = './swagger_output.json';
-const endpointsFiles = ["../index.js"]; 
 
-let doc ={
-    info:{
-        version:"1.0.0",
-        title: "API do BroadTasks",
-        description: "Documentação da API do BroadTasks."
-    },
-    servers:[
-        {
-        url: 'http://localhost:4000',
-        description: "Servidor Local - Local Host"
-    },
-    {
-        url: "https://boardtasks-back.vercel.app",
-        description: "Servidor de produção."
-    }
-    ],
-    consumes: ['application/json'],
-    produces: ['application/json']
+let outputFile = './swagger_output.json';
+let endpointsFiles = ['../index.js', '../src/routes.js'];
+
+
+if(String(process.env.OS).toLocaleLowerCase().includes("windows")){
+    outputFile = './swagger/swagger_output.json';
+    endpointsFiles = ['./index.js'];
 }
+
+
+let doc = {
+    info: {
+        version: "1.0.0",
+        title: "API do BoardTasks",
+        description: "Documentação da API do BoardTasks."
+    },
+    servers: [
+        {
+            url: "http://localhost:4000/",
+            description: "Servidor localhost."
+        },
+        {
+            url: "https://dnc-board-tasks-back.vercel.app/",
+            description: "Servidor de produção."
+        }
+    ],
+    //consumes: ['application/json'],
+    //produces: ['application/json'],
+    //components: {
+    //    schemas: {
+    //        Usuario: mongooseToSwagger(EsquemaUsuario),
+    //        Tarefa: mongooseToSwagger(EsquemaTarefa)
+    //    }
+    //}
+}
+
+
 swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-    console.log("Documentação do Swagger gerada encontra-se no arquivo em: "+ outputFile);
-    if(process.env.NODE_ENV !== 'production'){
+    console.log("Documentação do Swagger gerada encontra-se no arquivo em: " + outputFile);
+    if (process.env.NODE_ENV !== 'production') {
         require("../index.js");
     }
-
 })
