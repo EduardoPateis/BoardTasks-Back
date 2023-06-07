475bb22a4967f1cf0d69e58f84ccc9c81af45627
EduardoPateis <epjoaquim@outlook.com>
2023-06-06 22:07:14 -0300

    Criação do middleware de autenticação do Swagger

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const logger = require('morgan');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerOptions = { customCssUrl: '/swagger-ui.css' };
-//const authDocProducao = require('./src/middlewares/authDoc');
 const routes = require('./src/routes');
+const authDocProducao = require('./src/middlewares/authDoc');
 const app = express();
 require('dotenv').config();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 if(process.env.NODE_ENV !== 'test'){
     const swaggerFile = require('./swagger/swagger_output.json');
     app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc'); });
-    app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
+    app.use('/doc', authDocProducao, swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
 }
 
 
